cf88b2845caa24eff732a4d95e7175ebc5ead711
Michael Ala <michaelfonzolo@gmail.com>
2015-07-08 23:05:12 -0400

    Began work on Phosphaze multiforms.

---
+++ Phosphaze/Multiforms/IntroMultiform/MainMultiform.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Phosphaze.Framework;
+using Phosphaze.Framework.Cache;
+using Phosphaze.Framework.Collision;
+using Phosphaze.Framework.Display;
+using Phosphaze.Framework.Events;
+using Phosphaze.Framework.Extensions;
+using Phosphaze.Framework.Forms;
+using Phosphaze.Framework.Forms.Resources;
+using Phosphaze.Framework.Forms.Effectors;
+using Phosphaze.Framework.Forms.Effectors.Transitions;
+using Phosphaze.Framework.Input;
+using Phosphaze.Framework.Maths;
+using Phosphaze.Framework.Maths.Geometry;
+using Phosphaze.Framework.Timing;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Phosphaze.Multiforms.IntroMultiform
+{
+    public class MainMultiform : Multiform
+    {
+
+        public static string Background = "Background";
+
+        public override void Construct(Framework.ServiceLocator serviceLocator, MultiformData args)
+        {
+            var background = new TextureForm(
+                serviceLocator, "IntroContent/Backgrounds/MainBackground", new Vector2(0.5f, 0.5f));
+            background.Scale(0.9);
+            background.ToggleVisibility();
+            background.AddEffector(new InverseArcsineTransition(TextureForm.ALPHA_ATTR, 0.93, 4000));
+
+            RegisterForm(Background, background);
+
+            SetUpdater(Update);
+            SetRenderer(Render);
+        }
+
+        private void Update(ServiceLocator serviceLocator)
+        {
+            UpdateTime(serviceLocator);
+            UpdateForms(serviceLocator);
+
+            if (serviceLocator.Keyboard.IsReleased(Keys.Escape))
+                serviceLocator.Engine.Exit();
+
+            if (serviceLocator.Keyboard.IsReleased(Keys.Enter))
+                serviceLocator.DisplayManager.NextResolution();
+
+            var bg = (TextureForm)GetForm(Background);
+            if (At(4000))
+                bg.AddEffector(new FunctionalAttributeEffector(
+                    "Alpha", x => - 0.1 * Math.Pow(Math.Sin(x / 4000), 2.0)));
+
+            bg.SetPosition(0.5f + 0.045f*Math.Pow(Math.Sin(LocalTime/8000), 2.0), 0.49f);
+
+            var mpos = serviceLocator.Mouse.mousePosAsVec - Resolution.native.center;
+            var offset = new Vector2(0.000025f * mpos.X, 0.00001f * mpos.Y);
+            bg.Translate(offset);
+        }
+
+        private void Render(ServiceLocator serviceLocator)
+        {
+            serviceLocator.DisplayManager.SetSpriteBatchProperties(blendState: BlendState.Additive);
+            RenderForm(Background, serviceLocator);
+        }
+
+    }
+}
