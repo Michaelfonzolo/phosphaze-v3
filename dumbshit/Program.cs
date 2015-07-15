1ebd1c7d013253063c5eb14e1ed51803eb2da8fb
Michael Ala <michaelfonzolo@gmail.com>
2015-07-15 17:15:12 -0400

    Added dumbshit to test out dumbshit.

---
+++ dumbshit/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using NeonVM.Neon;
+
+namespace dumbshit
+{
+    class Program
+    {
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+
+            var s = new ShuntingYard(new List<string>(new string[] {  }));
+            s.Parse();
+            foreach (var i in s.GetInstructions())
+                Console.WriteLine(i);
+        }
+    }
+}
