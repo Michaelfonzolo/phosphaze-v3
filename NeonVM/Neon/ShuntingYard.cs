b82d9ed9f3e8062d074c2d2cf7725c2f8187e69c
Michael Ala <michaelfonzolo@gmail.com>
2015-07-15 21:18:16 -0400

    Reorganized code.

--- NeonVM/Neon/ShuntingYard.cs
+++ NeonVM/Neon/ShuntingYard.cs
@@ -25,6 +25,7 @@ namespace NeonVM.Neon
             public static VEC_START_INSTR Instance = new VEC_START_INSTR(); 
         }
 
+        // A dummy instruction representing the start of a relative vector.
         private class RVEC_START_INSTR : DUMMY_INSTR, IInstruction
         {
             public static RVEC_START_INSTR Instance = new RVEC_START_INSTR();
@@ -79,23 +80,72 @@ namespace NeonVM.Neon
             7, 8, 9, 10
         };
 
+        private static string[] BRACKET_TOKENS
+            = new string[] 
+        {
+            Tokens.EXPR_START,
+            Tokens.EXPR_END,
+            Tokens.ARRAY_START,
+            Tokens.ARRAY_END,
+            Tokens.DICT_START,
+            Tokens.DICT_END,
+            Tokens.BLOCK_START,
+            Tokens.BLOCK_END,
+            Tokens.VEC_START,
+            Tokens.VEC_END,
+            Tokens.RVEC_START,
+            Tokens.RVEC_END
+        };
+
+        private static Dictionary<string, Func<int, NeonSyntaxException>> mismatchedOpenBracketExceptions
+            = new Dictionary<string, Func<int, NeonSyntaxException>>()
+        {
+            {Tokens.EXPR_START, NeonExceptions.Exception0006},
+            {Tokens.VEC_START,  NeonExceptions.Exception0013},
+            {Tokens.RVEC_START, NeonExceptions.Exception0014}
+        };
+
         private Stack<string> tokenStack;
+
+        private Stack<IInstruction> operatorStack = new Stack<IInstruction>();
 
         private List<IInstruction> instructions = new List<IInstruction>();
 
         private int lineNumber = 0;
-        private Stack<IInstruction> operatorStack = new Stack<IInstruction>();
-        private Stack<int> exprStartLineNums = new Stack<int>();
-        private Stack<int> vecStartLineNums = new Stack<int>();
-        private Stack<int> rvecStartLineNums = new Stack<int>();
+
+        private Dictionary<string, Stack<int>> bracketLineStarts
+            = new Dictionary<string,Stack<int>>()
+        {
+            {Tokens.EXPR_START,  new Stack<int>()},
+            {Tokens.ARRAY_START, new Stack<int>()},
+            {Tokens.DICT_START,  new Stack<int>()},
+            {Tokens.BLOCK_START, new Stack<int>()},
+            {Tokens.VEC_START,   new Stack<int>()},
+            {Tokens.RVEC_START,  new Stack<int>()}
+        };
+
         private Stack<ParsingState> parsingStates = new Stack<ParsingState>();
+
         private ParsingState parsingState { get { return parsingStates.Peek(); } }
+
+        private Dictionary<string, Action> tokenActions;
 
         public ShuntingYard(List<string> tokens)
         {
             tokens.Reverse();
             tokenStack = new Stack<string>(tokens);
             parsingStates.Push(new ParsingState(ParsingStateType.Default));
+
+            tokenActions = new Dictionary<string, Action>()
+            {
+                {Tokens.EXPR_START, _Parse_EXPR_START},
+                {Tokens.EXPR_END,   _Parse_EXPR_END},
+                {Tokens.VEC_START,  _Parse_VEC_START},
+                {Tokens.VEC_END,    _Parse_VEC_END},
+                {Tokens.RVEC_START, _Parse_RVEC_START},
+                {Tokens.RVEC_END,   _Parse_RVEC_END},
+                {Tokens.ELEM_SEP,   _Parse_ELEM_SEP}
+            };
         }
 
         private static bool IsOp(string i)
@@ -262,17 +312,24 @@ namespace NeonVM.Neon
                         }
                         operatorStack.Push(op);
                     }
+                    else if (tokenActions.ContainsKey(token))
+                    {
+                        tokenActions[token]();
+                    }
                     else
                     {
-                        ParseFurther(token);
+                        // Change this later.
+                        throw new Exception();
                     }
                     prevToken = token;
                 }
             }
 
-            if (exprStartLineNums.Count != 0)
-                // Throw an exception for the first mismatched opening bracket discovered.
-                throw NeonExceptions.Exception0006(exprStartLineNums.Last());
+            foreach (var kvp in bracketLineStarts)
+            {
+                if (kvp.Value.Count != 0)
+                    throw mismatchedOpenBracketExceptions[kvp.Key](kvp.Value.Last());
+            }
 
             if (parsingState.Type == ParsingStateType.MultiLineComment)
                 // It doesn't matter if we end on a single line comment.
@@ -285,54 +342,81 @@ namespace NeonVM.Neon
             }
         }
 
-        private void ParseFurther(string token)
+        private void _Parse_EXPR_START()
         {
-            switch (token)
+            operatorStack.Push(EXPR_START_INSTR.Instance);
+            bracketLineStarts[Tokens.EXPR_START].Push(lineNumber);
+        }
+
+        private void _Parse_VEC_START()
+        {
+            BeginVec(
+                ParsingStateType.Vector, 
+                VEC_START_INSTR.Instance, 
+                bracketLineStarts[Tokens.VEC_START]
+                );
+        }
+
+        private void _Parse_RVEC_START()
+        {
+            BeginVec(
+                ParsingStateType.RelativeVector, 
+                RVEC_START_INSTR.Instance, 
+                bracketLineStarts[Tokens.RVEC_START]
+                );
+        }
+
+        private void _Parse_EXPR_END()
+        {
+            PopOpsUntil(
+                EXPR_START_INSTR.Instance,
+                bracketLineStarts[Tokens.EXPR_START],
+                NeonExceptions.Exception0005(lineNumber)
+                );
+        }
+
+        private void _Parse_VEC_END()
+        {
+            NeonSyntaxException _exc0010;
+            if (bracketLineStarts[Tokens.VEC_START].Count > 0)
+                _exc0010 = NeonExceptions.Exception0010(bracketLineStarts[Tokens.VEC_START].Peek());
+            else
+                _exc0010 = null;
+
+            EndVec(
+                VEC_START_INSTR.Instance,
+                bracketLineStarts[Tokens.VEC_START],
+                NeonExceptions.Exception0009(lineNumber),
+                NeonExceptions.Exception0010(lineNumber),
+                BUILD_VEC.Instance
+                );
+        }
+
+        private void _Parse_RVEC_END()
+        {
+            NeonSyntaxException _exc0012;
+            if (bracketLineStarts[Tokens.RVEC_START].Count > 0)
+                _exc0012 = NeonExceptions.Exception0012(bracketLineStarts[Tokens.RVEC_START].Peek());
+            else
+                _exc0012 = null;
+
+            EndVec(
+                RVEC_START_INSTR.Instance,
+                bracketLineStarts[Tokens.RVEC_START],
+                NeonExceptions.Exception0011(lineNumber),
+                NeonExceptions.Exception0012(lineNumber),
+                BUILD_RVEC.Instance
+                );
+        }
+
+        private void _Parse_ELEM_SEP()
+        {
+            switch (parsingState.Type)
             {
-                case Tokens.EXPR_START:
-                    operatorStack.Push(EXPR_START_INSTR.Instance);
-                    exprStartLineNums.Push(lineNumber);
-                    break;
-                case Tokens.VEC_START:
-                    BeginVec(ParsingStateType.Vector, VEC_START_INSTR.Instance, vecStartLineNums);
-                    break;
-                case Tokens.RVEC_START:
-                    BeginVec(ParsingStateType.RelativeVector, RVEC_START_INSTR.Instance, rvecStartLineNums);
-                    break;
-                case Tokens.EXPR_END:
-                    PopOpsUntil(
-                        EXPR_START_INSTR.Instance,
-                        exprStartLineNums,
-                        NeonExceptions.Exception0005(lineNumber));
-                    break;
-                case Tokens.VEC_END:
-                    EndVec(
-                        VEC_START_INSTR.Instance,
-                        vecStartLineNums,
-                        NeonExceptions.Exception0009(lineNumber),
-                        NeonExceptions.Exception0010(vecStartLineNums.Peek()),
-                        BUILD_VEC.Instance);
-                    break;
-                case Tokens.RVEC_END:
-                    EndVec(
-                        RVEC_START_INSTR.Instance,
-                        rvecStartLineNums,
-                        NeonExceptions.Exception0011(lineNumber),
-                        NeonExceptions.Exception0012(lineNumber),
-                        BUILD_RVEC.Instance);
-                    break;
-                case Tokens.ELEM_SEP:
-                    switch (parsingState.Type)
-                    {
-                        case ParsingStateType.Vector:
-                        case ParsingStateType.RelativeVector:
-                            parsingState.Attributes["componentCount"]
-                                = (int)(parsingState.Attributes["componentCount"]) + 1;
-                            break;
-                        default:
-                            // Change this later.
-                            throw new Exception();
-                    }
+                case ParsingStateType.Vector:
+                case ParsingStateType.RelativeVector:
+                    parsingState.Attributes["componentCount"]
+                        = (int)(parsingState.Attributes["componentCount"]) + 1;
                     break;
                 default:
                     // Change this later.
