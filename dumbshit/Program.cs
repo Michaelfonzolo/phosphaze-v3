d6e534af728eba6940e8e9fc1e89ca3dbe3a6d38
Michael Ala <michaelfonzolo@gmail.com>
2015-07-19 20:17:33 -0400

    Renamed NeonTests -> NeonVMTests, added NUnit and SystemWrapper references to unit testing projects.

--- dumbshit/Program.cs
+++ dumbshit/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -14,6 +16,8 @@ namespace dumbshit
         [STAThread]
         static void Main(string[] args)
         {
+            Console.WriteLine(Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName));
+            Console.WriteLine(AppDomain.CurrentDomain.BaseDirectory);
             var s = new ShuntingYardParser(new List<string>(new string[] { "ORIGIN", "+", "<|", "1", ",", "<<", "3", ",", "5", ">>", "|>" }));
             s.Parse();
             foreach (var i in s.GetInstructions())
